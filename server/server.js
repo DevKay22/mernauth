fbabad6580677a594bcbbbc1e9afa429b7106dbb
Kayode <drkay4sure@gmail.com>
2025-06-01 23:37:19 +0100

    serverjs setup

---
+++ server/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import 'dotenv/config';
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({credentials: true}));
+
+
+app.get("/", (req, res)=> {
+    res.send("api is working fine");
+})
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
+})
+
