2897633cdcadbd1bfe163a0b3b3c756d3a33dd77
Kayode <drkay4sure@gmail.com>
2025-06-02 01:11:24 +0100

    logout controller

--- server/Controllers/authController.js
+++ server/Controllers/authController.js
@@ -40,3 +40,64 @@ export const register = async (req, res) => {
         res.json({ success: false, message: error.message });
     }
 };
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.json({ success: false, message: 'Email and password are required' });
+    }
+
+    try {
+        const user = await userModel.findOne({ email });
+
+        if (!user) {
+            return res.json({ success: false, message: 'Invalid email' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.json({ success: false, message: 'Invalid password' });
+        }
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '3d' });
+
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            maxAge: 3 * 24 * 60 * 60 * 1000
+        });
+
+        return res.json({
+            success: true,
+            message: "Login successful",
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email
+            }
+        });
+
+    } catch (error) {
+        return res.json({ success: false, message: error.message });
+    }
+};
+
+
+// logout
+export const logout = async (req, res) => {
+
+    try {
+       res.clearCookie('token', {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        });
+        return res.json({ success: true, message: "Logged out" }); 
+    } catch (error) {
+       return res.json({ success: false, message: error.message });
+    }
+    
+};
