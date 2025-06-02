40cee1b502267e82af1914ed70834afc1019eeb0
Kayode <drkay4sure@gmail.com>
2025-06-02 01:25:55 +0100

    authroutes

--- server/server.js
+++ server/server.js
@@ -8,15 +8,17 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 connectDB();
+import authRouter from './Routes/authRoutes.js'
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({credentials: true}));
 
-
+// API endpoints
 app.get("/", (req, res)=> {
     res.send("api is working fine");
 })
+app.use('/api/auth', authRouter )
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`)
 })
